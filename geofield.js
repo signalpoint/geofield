6b60673593a6511bd6aa0b5d0ecceb73b568c6fb
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-02-03 18:17:51 -0500

    Changed 'lng' to 'lon' and utilized the new field_key parameter with geofield_assemble_form_state_into_field().

--- geofield.js
+++ geofield.js
@@ -24,19 +24,34 @@ function geofield_field_widget_form(form, form_state, field, instance, langcode,
     // For a latitude/longitude widget, we create two text fields and a button
     // to get the current position and fill in the two text fields.
     if (instance.widget.type == 'geofield_latlon') {
+      var onchange = '_geofield_field_widget_form_change(this, \'' + items[delta].id + '\')';
+      var lat_id = items[delta].id + '-lat';
       var lat = {
-        id: items[delta].id + '-lat',
+        id: lat_id,
         title: 'Latitude',
-        type: 'textfield'
+        type: 'textfield',
+        options: {
+          attributes: {
+            id: lat_id,
+            onchange: onchange
+          }
+        }
       };
-      var lng = {
-        id: items[delta].id + '-lng',
+      var lon_id = items[delta].id + '-lon';
+      var lon = {
+        id: lon_id,
         title: 'Longitude',
-        type: 'textfield'
+        type: 'textfield',
+        options: {
+          attributes: {
+            id: lon_id,
+            onchange: onchange
+          }
+        }
       };
       var options = {
         lat: lat.id,
-        lng: lng.id
+        lon: lon.id
       };
       var btn = {
         id: items[delta].id + '-btn',
@@ -44,12 +59,12 @@ function geofield_field_widget_form(form, form_state, field, instance, langcode,
         type: 'button',
         options: {
           attributes: {
-            onclick: '_geofield_field_widget_form_click(\'' + lat.id + '\', \'' + lng.id + '\')'
+            onclick: '_geofield_field_widget_form_click(\'' + lat.id + '\', \'' + lon.id + '\')'
           }
         }
       };
       items[delta].children.push(lat);
-      items[delta].children.push(lng);
+      items[delta].children.push(lon);
       items[delta].children.push(btn);
     }
     else {
@@ -62,12 +77,34 @@ function geofield_field_widget_form(form, form_state, field, instance, langcode,
 /**
  *
  */
-function _geofield_field_widget_form_click(lat_id, lng_id) {
+function _geofield_field_widget_form_change(textfield, input) {
+  try {
+    // Depending on which textfield just changed values, grab the other one as
+    // well, then build the lat,lon value for the hidden input.
+    var which = $(textfield).attr('id');
+    var other = which;
+    if (which.indexOf('-lat') != -1) { other = which.replace('-lat', '-lon'); }
+    else {
+      other = which.replace('-lon', '-lat');
+      var swap = other;
+      other = which;
+      which = swap;
+    }
+    var value = $('#' + which).val() + ',' + $('#' + other).val();
+    $('#' + input).val(value);
+  }
+  catch (error) { console.log('_geofield_field_widget_form_change - ' + error); }
+}
+
+/**
+ *
+ */
+function _geofield_field_widget_form_click(lat_id, lon_id) {
   try {
     navigator.geolocation.getCurrentPosition(
       function(position) {
         $('#' + lat_id).val(position.coords.latitude);
-        $('#' + lng_id).val(position.coords.longitude);
+        $('#' + lon_id).val(position.coords.longitude);
       },
       function(error) {
         console.log('_geofield_field_widget_form_click - getCurrentPosition - ' + error);
@@ -78,5 +115,27 @@ function _geofield_field_widget_form_click(lat_id, lng_id) {
     );
   }
   catch (error) { console.log('_geofield_field_widget_form_click - ' + error); }
+}
+
+/**
+ * Implements hook_assemble_form_state_into_field().
+ */
+function geofield_assemble_form_state_into_field(entity_type, bundle,
+  form_state_value, field, instance, langcode, delta, field_key) {
+  try {
+    if (empty(form_state_value)) { return null; }
+    var coordinates = form_state_value.split(',');
+    if (coordinates.length != 2) { return null; }
+    // We don't want to use a key for this item's value.
+    field_key.use_key = false;
+    // Return the assembled value.
+    return {
+      lat: coordinates[0],
+      lon: coordinates[1]
+    };
+  }
+  catch (error) {
+    console.log('geofield_assemble_form_state_into_field - ' + error);
+  }
 }
 
